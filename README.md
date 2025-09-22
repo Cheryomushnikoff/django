aab1048b85fe7fa90ca00b1517e4130139a53d00
SergeiCH <cheremushnikoff@yandex.ru>
2025-09-22 12:33:20 +0800

    создал юзер

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-'''bash
-python3 -m venv .venv
+## Проект на Django
+
+
+### Установка зависимостей
+```bash
+python -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
-'''
+```
+
+### Применение миграций 
+```bash
+python manage.py migrate
+```
--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, ) # verbose_name=
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    text = models.TextField(verbose_name="Содержание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
 
     class Meta:
         db_table = 'blog_posts'
+        verbose_name_plural = 'Посты'
+        verbose_name = 'Пост'
+
+    def __str__(self):
+        return self.title
+
+class User(models.Model):
+    name = models.CharField(max_length=30, verbose_name='Имя')
+    surname = models.CharField(max_length=30, verbose_name='Фамилия')
+    created_at =models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Пользователи'
+        verbose_name = 'Пользователь'
+        db_table = 'blog_users'
+
