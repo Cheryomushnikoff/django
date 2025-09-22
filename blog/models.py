3534fc9abd9d895dd93886ea7f811b355160cd53
SergeiCH <cheremushnikoff@yandex.ru>
2025-09-22 10:05:18 +0800

    Создал модель Поста, создал и применил миграции

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, ) # verbose_name=
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'blog_posts'
