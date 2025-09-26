4278967aca0e6cad87617ab6b8e5d49550d8311b
SergeiCH <cheremushnikoff@yandex.ru>
2025-09-26 15:34:08 +0800

    Создал механизм создания поста, самым простым методом

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 
 
@@ -18,3 +18,12 @@ def get_post_detail(request, post_id):
     }
 
     return render(request, 'blog/post_detail.html', context)
+
+def create_post(request):
+    if request.method == 'GET':
+        return render(request, 'blog/post_add.html')
+
+    if request.method == 'POST':
+        post = Post.objects.create(title=request.POST.get('titlePost'), text=request.POST.get('textPost'))
+
+        return redirect('post_detail', post_id=post.id )
